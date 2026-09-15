ef1a8b0f3d48be95e3fdd928c2730ed8fdbbfabe
Dave Beusing <david.beusing@gmail.com>
2026-09-15 22:34:50 +0200

    Use production Operator remote transport

--- src/Hosts/rtaime.Operator/MainWindow.xaml.cs
+++ src/Hosts/rtaime.Operator/MainWindow.xaml.cs
@@ -1,18 +1,26 @@
+// Copyright (c) Dave Beusing <david.beusing@gmail.com>.
+
 using System.Windows;
+using rtaime.Client;
 
 namespace rtaime.Operator;
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
-    {
-        InitializeComponent();
-        DataContext = new OperatorViewModel();
-    }
+	public MainWindow()
+	{
+		InitializeComponent();
+		var endpoint = Environment.GetEnvironmentVariable("RTAIME_CONTROL_ENDPOINT");
+		if (string.IsNullOrWhiteSpace(endpoint))
+			endpoint = "rtaime.v1.control.default";
 
-    public MainWindow(OperatorViewModel viewModel)
-    {
-        InitializeComponent();
-        DataContext = viewModel ?? throw new ArgumentNullException(nameof(viewModel));
-    }
+		var transport = new NamedPipeOperatorControlTransport(endpoint);
+		DataContext = new OperatorViewModel(new OperatorControlClient(transport));
+	}
+
+	public MainWindow(OperatorViewModel viewModel)
+	{
+		InitializeComponent();
+		DataContext = viewModel ?? throw new ArgumentNullException(nameof(viewModel));
+	}
 }
