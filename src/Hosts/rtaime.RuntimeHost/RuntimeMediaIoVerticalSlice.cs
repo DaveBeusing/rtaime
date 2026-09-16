844f85b13794ebd8710fcef006563f8e1b0576e1
Dave Beusing <david.beusing@gmail.com>
2026-09-16 18:37:58 +0200

    Bridge Media I/O frames into GPU composition at RuntimeHost boundary

--- src/Hosts/rtaime.RuntimeHost/RuntimeMediaIoVerticalSlice.cs
+++ src/Hosts/rtaime.RuntimeHost/RuntimeMediaIoVerticalSlice.cs
@@ -2,6 +2,7 @@
 
 using rtaime.Media;
 using rtaime.Media.Contracts;
+using rtaime.Provider.Gpu;
 
 namespace rtaime.RuntimeHost;
 
@@ -82,9 +83,11 @@ internal sealed class RuntimeMediaIoVerticalSlice : IDisposable
 	{
 		if (capture is null || appliedSequence == capture.CaptureSequence)
 			return;
+
+		var gpuInput = new RgbaFrameBuffer(capture.Video.Format, capture.Video.Pixels.Span);
 		_runtime.SetExternalInputContent(
 			capture.RuntimeSourceId,
-			capture.Video,
+			gpuInput,
 			MapSignal(capture.PortStatus.SignalState));
 		appliedSequence = capture.CaptureSequence;
 	}
