f6aa69515972e47dfaf0471df59b7b10a111febf
Dave Beusing <david.beusing@gmail.com>
2026-09-18 20:30:38 +0200

    AP-54 fix provider health projection import

--- src/Hosts/rtaime.ControlHost/ControlHostIpcServer.cs
+++ src/Hosts/rtaime.ControlHost/ControlHostIpcServer.cs
@@ -6,6 +6,7 @@ using System.Text.Json;
 using rtaime.Control.Contracts;
 using rtaime.Core;
 using rtaime.Media.Contracts;
+using rtaime.Provider.Contracts;
 using rtaime.Runtime.Contracts;
 
 namespace rtaime.ControlHost;
