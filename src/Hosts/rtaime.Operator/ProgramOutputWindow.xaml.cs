e2fa65e0f93b0410a54f6364b45a7f21bf305fc2
Dave Beusing <david.beusing@gmail.com>
2026-09-18 18:21:40 +0200

    AP-51 add clean feed window code-behind

---
+++ src/Hosts/rtaime.Operator/ProgramOutputWindow.xaml.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Dave Beusing <david.beusing@gmail.com>.
+
+using System.Windows;
+
+namespace rtaime.Operator;
+
+public partial class ProgramOutputWindow : Window
+{
+	public ProgramOutputWindow()
+	{
+		InitializeComponent();
+	}
+}
