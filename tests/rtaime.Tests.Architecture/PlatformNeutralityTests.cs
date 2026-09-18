dcf02dfa80d981f071e7ca425056d40d426a576c
Dave Beusing <david.beusing@gmail.com>
2026-09-19 00:51:11 +0200

    Align package boundary test with framework-provided pipes

--- tests/rtaime.Tests.Architecture/PlatformNeutralityTests.cs
+++ tests/rtaime.Tests.Architecture/PlatformNeutralityTests.cs
@@ -27,14 +27,6 @@ public sealed class PlatformNeutralityTests
 			["rtaime.AppHost"] = new HashSet<string>(StringComparer.Ordinal)
 			{
 				"Microsoft.Extensions.Hosting.WindowsServices"
-			},
-			["rtaime.ControlHost"] = new HashSet<string>(StringComparer.Ordinal)
-			{
-				"System.IO.Pipes.AccessControl"
-			},
-			["rtaime.RuntimeHost"] = new HashSet<string>(StringComparer.Ordinal)
-			{
-				"System.IO.Pipes.AccessControl"
 			}
 		};
 
