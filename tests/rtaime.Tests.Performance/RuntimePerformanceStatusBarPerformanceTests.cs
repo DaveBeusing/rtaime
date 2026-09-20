00f7d462cf8513ae13ed77286d9042251b0df40f
Dave Beusing <david.beusing@gmail.com>
2026-09-20 16:54:04 +0200

    Add output cadence allocation smoke test

---
+++ tests/rtaime.Tests.Performance/RuntimePerformanceStatusBarPerformanceTests.cs
@@ -0,0 +1,26 @@
+// Copyright (c) Dave Beusing <david.beusing@gmail.com>.
+
+using rtaime.RuntimeHost;
+
+namespace rtaime.Tests.Performance;
+
+public sealed class RuntimePerformanceStatusBarPerformanceTests
+{
+	[Fact]
+	public void Program_cadence_measurement_allocates_no_per_frame_history()
+	{
+		var counter = new RuntimeFrameDropCounter();
+		var framePeriod = TimeSpan.FromMilliseconds(20);
+
+		for (var index = 0; index < 128; index++)
+			counter.Observe(TimeSpan.FromTicks(index * framePeriod.Ticks), framePeriod);
+
+		var start = GC.GetAllocatedBytesForCurrentThread();
+		for (var index = 128; index < 100_128; index++)
+			counter.Observe(TimeSpan.FromTicks(index * framePeriod.Ticks), framePeriod);
+		var allocated = GC.GetAllocatedBytesForCurrentThread() - start;
+
+		Assert.Equal(0, allocated);
+		Assert.Equal(50, counter.OutputFramesPerSecond, 6);
+	}
+}
