78fb6c7c54643aa65e9bea9c8c9364b559de6dc1
Dave Beusing <david.beusing@gmail.com>
2026-09-20 16:51:52 +0200

    Add Runtime statusbar control shell

---
+++ src/Hosts/rtaime.Operator/RuntimePerformanceStatusBarControl.xaml.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Dave Beusing <david.beusing@gmail.com>.
+
+using System.Windows.Controls;
+
+namespace rtaime.Operator;
+
+public partial class RuntimePerformanceStatusBarControl : UserControl
+{
+	public RuntimePerformanceStatusBarControl()
+	{
+		InitializeComponent();
+	}
+}
