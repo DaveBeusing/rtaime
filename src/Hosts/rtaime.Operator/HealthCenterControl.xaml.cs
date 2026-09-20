54c2d91ed3a4e118717cb0fe5d9b5df3f823e706
Dave Beusing <david.beusing@gmail.com>
2026-09-20 14:11:40 +0200

    Add Health Center control host

---
+++ src/Hosts/rtaime.Operator/HealthCenterControl.xaml.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Dave Beusing <david.beusing@gmail.com>.
+
+using System.Windows.Controls;
+
+namespace rtaime.Operator;
+
+public partial class HealthCenterControl : UserControl
+{
+	public HealthCenterControl()
+	{
+		InitializeComponent();
+	}
+}
